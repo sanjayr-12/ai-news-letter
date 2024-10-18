e34a23cef30ceae185019cf5f75ac480c5e18ee5
sanjayr-12 <sanjayr.bit22@gmail.com>
2024-10-18 13:10:33 +0530

    prompt updated

--- server/gemini/gemini.js
+++ server/gemini/gemini.js
@@ -7,7 +7,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 export async function genContent() {
   try {
     const prompt =
-      "choose any two random tech topics, the first one should be the programming topic and the second is technology oriented topic and write each content in 200 words, there should be only the heading and the content, and no other words and return in the html format, note each time return the some unique topic";
+      "your task is to pick any random two topics about programming or technology and write about it in 200words for each topic in family guy peter griffen style and give it in the html format";
     const result = await model.generateContent(prompt);
     return result.response.text();
   } catch (error) {
