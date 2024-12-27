249ed6d8dfb0dc743361d264bdd876e42ed76528
sanjayr-12 <sanjayr.bit22@gmail.com>
2024-12-27 11:57:13 +0530

    updated goodbye mail

--- server/utils/goodBye.js
+++ server/utils/goodBye.js
@@ -13,7 +13,7 @@ export async function GoodByeMess(mail) {
       subject: "Sad to See You Go!",
       text: `Oh man, you unsubscribed? That’s kinda sad... I thought we had something special! But hey, no hard feelings. If you ever feel like coming back and getting two random tech topics every day at 11:10 AM, you can resubscribe here: https://www.peter.work.gd. Anyway, take care... I’ll miss ya. Heh heh.`,
       html: `<p>Oh man, you unsubscribed? That’s kinda sad... I thought we had something special! But hey, no hard feelings.</p>
-             <p>If you ever feel like coming back and getting <b>two random tech topics</b> every day at <b>11:10 AM</b>, you can resubscribe here: <a href="https://www.peter.work.gd" target="_blank">https://www.peter.work.gd</a>.</p>
+             <p>If you ever feel like coming back and getting <b>two random tech topics</b> every day at <b>11:10 AM</b>, you can resubscribe here: <a href="https://www.omgpeter.tech" target="_blank">https://www.omgpeter.tech</a>.</p>
              <p>Anyway, take care... I’ll miss ya. Heh heh.</p>`,
     });
   } catch (err) {
