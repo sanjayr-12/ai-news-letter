4d73ac8c122bb025b9776b7c62c6395e10f9b39b
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-01-04 14:16:47 +0530

    updated thank mail

--- server/utils/thank.mail.js
+++ server/utils/thank.mail.js
@@ -11,8 +11,8 @@ export async function ThankMess(mail) {
       },
       to: mail,
       subject: "Thanks for Subscribing, Pal!",
-      text: `Hey, thanks for subscribing or whatever. So here’s the deal—every day at 11:10 AM, you’re gonna get two random tech topics. Yeah, sounds like a lot of work for you, but don’t worry, you don’t have to do much except read them. 11:10 AM, every day. Maybe grab some coffee. Heh heh, good luck with that!`,
-      html: `<p>Hey, thanks for subscribing or whatever. Here’s the deal—<b>every day at 11:10 AM</b>, you’re gonna get <b>two random tech topics</b>. Sounds like a lot, right? But don’t worry, all you gotta do is read ‘em. 11:10 AM sharp, every day. Maybe grab a coffee or something... Heh heh, good luck with that!</p>`,
+      text: `Hey, thanks for subscribing or whatever. But here’s the thing—I’m kinda broke right now, so no tech topics for a while. Yeah, bummer, huh? I’m taking a break. Don’t worry though, I’ll let you know when I’m back in action! Heh heh, stay tuned!`,
+      html: `<p>Hey, thanks for subscribing or whatever. But here’s the thing—<b>I’m kinda broke right now</b>, so no tech topics for a while. Yeah, bummer, huh? <b>I’m taking a break</b>. Don’t worry though, I’ll let you know when I’m back in action! Heh heh, stay tuned!</p>`,
     });
     console.log("Thank you email sent successfully!");
   } catch (err) {
