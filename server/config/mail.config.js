d644704fe4cfbbe7ed0ab821f398c53e386f0c43
sanjayr-12 <sanjayr.bit22@gmail.com>
2025-01-26 21:08:21 +0530

    again changed the smtp :)

--- server/config/mail.config.js
+++ server/config/mail.config.js
@@ -2,9 +2,10 @@ import nodemailer from "nodemailer";
 import { configDotenv } from "dotenv";
 configDotenv();
 export const transporter = nodemailer.createTransport({
-    host: "mail.privateemail.com",
-    port: 465,
-    secure: true,
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.USER,
         pass: process.env.PASS
